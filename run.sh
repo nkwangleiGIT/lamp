12a66c2d5c929262ede33f1c2ee40df666e743db
Wang Lei <wanglei@tenxcloud.com>
2016-03-18 12:28:49 +0800

    p1

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "test"
+
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
 
